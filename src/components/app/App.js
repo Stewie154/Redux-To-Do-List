1ff73e494cbd6b6012911babcf2277b52f53fcbf
Stewart McFarlane <stewart@toru.digital>
2022-06-20 22:01:23 +0100

    added container class to App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,9 @@ import '../../index.css'
 
 const App = () => {
   return (
-    <div className="bg-red-500">App</div>
+    <div className="bg-red-500 container mx-auto px-5">
+        App
+    </div>
   )
 }
 
