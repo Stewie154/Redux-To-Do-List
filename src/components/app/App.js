ab83b44c361eecfa8fb67020e886276e686bbdbc
Stewart McFarlane <stewart@toru.digital>
2022-06-25 18:46:00 +0100

    created Header component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import '../../index.css'
 import '../../styles/global.scss'
 
+import Header from '../header/Header'
 import ToDoList from '../todo-list/ToDoList'
+
 
 const App = () => {
 	return (
 		<div className="background-primary w-screen h-screen flex justify-center items-center">
-			<main className="container-background-color w-[750px] max-w-[90%] h-[700px] py-10 px-5 md:py-20 md:px-10  overflow-scroll mx-auto border-color-secondary rounded-lg">
+			<main className="container-background-color w-[750px] max-w-[90%] h-[700px] py-10 px-5 md:py-20 md:px-10  overflow-scroll mx-auto border-color-secondary rounded-lg text-center">
+				<Header />
 				<ToDoList/>
 			</main>	
 		</div>
---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 className="text-3xl md:text-6xl underline">Stewart's To Do List</h1>
+  )
+}
+
+export default Header
