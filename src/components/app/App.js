bf3eb1e1e6b2da91daf57aa0f99aed18e46af3b8
Stewart McFarlane <stewart@toru.digital>
2022-06-25 18:23:15 +0100

    added sass and created variables & global styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../../index.css'
+import '../../styles/global.scss'
+
+import ToDoList from '../todo-list/ToDoList'
 
 const App = () => {
   return (
-    <div className="bg-red-500 container mx-auto px-5">
+    <div className="background-primary container mx-auto px-5">
         App
     </div>
   )
