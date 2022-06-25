b619011e70b6888217c5538e01ffa8af5be7c50e
Stewart McFarlane <stewart@toru.digital>
2022-06-25 19:46:32 +0100

    retrieved to do items from redux store and logged them to the console

--- src/components/to-do-list/ToDoList.js
+++ src/components/to-do-list/ToDoList.js
@@ -3,6 +3,8 @@ import { fetchAllToDos } from '../../redux/actions/toDos'
 import { useSelector } from 'react-redux'
 
 const ToDoList = () => {
+	const allToDos = useSelector(state => state.toDos)
+	console.log(allToDos)
 
 	useEffect(() => {
 		fetchAllToDos()
