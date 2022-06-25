8f4616ef40ee499a630c647e574cbe9d783a0765
Stewart McFarlane <stewart@toru.digital>
2022-06-25 19:31:09 +0100

    added line to store to get redux devtools working

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,7 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import combinedReducers from '../reducers/index'
 
-export default createStore(combinedReducers)
+export default createStore(
+    combinedReducers, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
