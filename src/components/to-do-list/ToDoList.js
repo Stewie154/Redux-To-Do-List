755156966c6f9942614b9c3ddb7a6b9980261a5a
Stewart McFarlane <stewart@toru.digital>
2022-06-25 19:49:56 +0100

    removed useEffect for now as it's not needed to retrieve the state from the store

--- src/components/to-do-list/ToDoList.js
+++ src/components/to-do-list/ToDoList.js
@@ -1,14 +1,10 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { fetchAllToDos } from '../../redux/actions/toDos'
 import { useSelector } from 'react-redux'
 
 const ToDoList = () => {
 	const allToDos = useSelector(state => state.toDos)
 	console.log(allToDos)
-
-	useEffect(() => {
-		fetchAllToDos()
-	}, [])
 
 	return (
 		<div>ToDoList</div>
