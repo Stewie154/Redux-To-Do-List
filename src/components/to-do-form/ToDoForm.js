e3b4861b4fd67fce754fd17ac78234d55fa8b717
Stewart McFarlane <stewart@toru.digital>
2022-06-26 12:24:43 +0100

    submit button styles

--- src/components/to-do-form/ToDoForm.js
+++ src/components/to-do-form/ToDoForm.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const ToDoForm = () => {
 	return (
-		<form className="absolute bottom-0 left-0 w-full py-8 px-4 border-t border-color-secondary">
+		<form className="absolute bottom-0 left-0 w-full py-8 px-4 flex justify-between items-between border-t border-color-secondary">
 			<input 
 				type="text" 
 				placeholder="Enter to do task..." 
-				className="w-full bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
+				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
 			/>
+			<button className="border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">Add Task</button>
 		</form>
 	)
 }
