0dcdca634c5bc7f93eaad1baf18879302a73794b
Stewart McFarlane <stewart@toru.digital>
2022-06-26 12:03:47 +0100

    created ToDoForm component and positioned it at the bottom of the container

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,14 +4,16 @@ import '../../styles/global.scss'
 
 import Header from '../header/Header'
 import ToDoList from '../to-do-list/ToDoList'
+import ToDoForm from '../to-do-form/ToDoForm'
 
 
 const App = () => {
 	return (
 		<div className="background-primary w-screen h-screen flex justify-center items-center">
-			<main className="container-background-color w-[750px] max-w-[90%] h-[700px] py-10 px-5 md:py-20 md:px-10  overflow-scroll mx-auto border-color-secondary rounded-lg text-center">
+			<main className="relative container-background-color w-[750px] max-w-[90%] h-[700px] py-10 px-5 md:py-20 md:px-10  overflow-scroll mx-auto border border-color-secondary rounded-lg text-center">
 				<Header />
 				<ToDoList/>
+				<ToDoForm />
 			</main>	
 		</div>
 	)
---
+++ src/components/to-do-form/ToDoForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToDoForm = () => {
+  return (
+    <div className="absolute bottom-0 left-0 w-full border-t border-color-secondary">ToDoForm</div>
+  )
+}
+
+export default ToDoForm
