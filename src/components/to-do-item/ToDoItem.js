2629e89bf056bf046f7da60a8fd50bcdd21657c2
Stewart McFarlane <stewart@toru.digital>
2022-06-26 11:44:53 +0100

    added green check icon

--- src/components/to-do-item/ToDoItem.js
+++ src/components/to-do-item/ToDoItem.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const ToDoItem = ({ item }) => {
-  return (
-    <div>{item.title}</div>
-  )
+	return (
+	<div className="flex justify-between">
+		<p>{item.title}</p>
+		<div className="flex">
+			<img 
+				src="/images/icons/checkmark-circle-outline.svg" 
+				alt="check icon"
+				className="w-6"
+			/>
+		</div>
+	</div>
+	)
 }
 
 export default ToDoItem
