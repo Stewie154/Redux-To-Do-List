448c5bec53584507b3a1d13f0039d5f29886922c
Stewart McFarlane <stewart@toru.digital>
2022-06-26 12:41:12 +0100

    temporarily removed value and created handle change function, console logged text input value

--- src/components/to-do-form/ToDoForm.js
+++ src/components/to-do-form/ToDoForm.js
@@ -4,14 +4,17 @@ import { useSelector } from 'react-redux'
 const ToDoForm = () => {
 
 	const currentText = useSelector(state => state.currentText)
-	
+	const handleChange = (value) => {
+		console.log(value)
+	}
 	return (
 		<form className="absolute bottom-0 left-0 w-full py-8 px-4 flex justify-between items-between border-t border-color-secondary">
 			<input 
 				type="text" 
-				value={currentText}
+				
 				placeholder="Enter to do task..." 
 				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
+				onChange={ (e) => { handleChange(e.target.value) } }
 			/>
 			<button className="border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">
 				Add Task
