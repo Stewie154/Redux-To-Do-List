0353edbecb594b00926c4b1c604dfe9bdd78f768
Stewart McFarlane <stewart@toru.digital>
2022-06-26 12:37:18 +0100

    stored current text in redux store, value property of form input assigned to to current text

--- src/components/to-do-form/ToDoForm.js
+++ src/components/to-do-form/ToDoForm.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ToDoForm = () => {
+
+	const currentText = useSelector(state => state.currentText)
+	
 	return (
 		<form className="absolute bottom-0 left-0 w-full py-8 px-4 flex justify-between items-between border-t border-color-secondary">
 			<input 
 				type="text" 
+				value={currentText}
 				placeholder="Enter to do task..." 
 				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
 			/>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import toDoReducer from './toDoReducer'
 import userReducer from './userReducer'
+import toDoFormReducer from "./toDoFormReducer"
 
 export default combineReducers({
     user: userReducer,
-    toDos: toDoReducer
+    toDos: toDoReducer,
+    currentText: toDoFormReducer
 })
