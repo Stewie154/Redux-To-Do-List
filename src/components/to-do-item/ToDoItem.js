d57c2036576e50fd610ad86376df2271af2e48e0
Stewart McFarlane <stewart@toru.digital>
2022-06-26 11:52:42 +0100

    icon hover styles

--- src/components/to-do-item/ToDoItem.js
+++ src/components/to-do-item/ToDoItem.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const ToDoItem = ({ item }) => {
 	return (
-	<div className="flex justify-between">
+	<div className="flex justify-between items-center py-2">
 		<p>{item.title}</p>
 		<div className="flex">
 			<img 
 				src="/images/icons/checkmark-circle-outline.svg" 
 				alt="check icon"
-				className="w-6"
+				className="w-8 cursor-pointer mr-3 transition-all duration-150 ease-out hover:ease-in hover:w-10 hover:opacity-70"
 			/>
 			<img 
 				src="/images/icons/trash-outline.svg" 
 				alt="check icon"
-				className="w-6"
+				className="w-8 cursor-pointer transition-all duration-150 ease-out hover:ease-in hover:w-10 hover:opacity-70"
 			/>
 		</div>
 	</div>
