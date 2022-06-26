f3147df3d02898dfefb92c41a69fa9d5db2baf7a
Stewart McFarlane <stewart@toru.digital>
2022-06-26 12:25:29 +0100

    format

--- src/components/to-do-form/ToDoForm.js
+++ src/components/to-do-form/ToDoForm.js
@@ -8,7 +8,9 @@ const ToDoForm = () => {
 				placeholder="Enter to do task..." 
 				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
 			/>
-			<button className="border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">Add Task</button>
+			<button className="border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">
+				Add Task
+			</button>
 		</form>
 	)
 }
