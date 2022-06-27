3e959709d763a5355cd9987578670c97a4da56c8
Stewart McFarlane <stewart@toru.digital>
2022-06-27 21:08:36 +0100

    used useRef inside useEffect to auto focus on text input on component mount

--- src/components/to-do-form/ToDoForm.js
+++ src/components/to-do-form/ToDoForm.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { updateText } from '../../redux/actions/toDoForm'
 import { createToDo } from '../../redux/actions/toDos'
 
 const ToDoForm = () => {
-
 	const currentText = useSelector(state => state.currentText)
 	const dispatch = useDispatch()
+
+    const textInput = useRef(null)
 
 	const handleChange = (textValue) => {
 		dispatch(updateText(textValue))
@@ -22,6 +23,10 @@ const ToDoForm = () => {
 		}
 	}
 
+    useEffect(() => {
+        textInput.current.focus()
+    }, [])
+
 	return (
 		<form 
 			className="absolute bottom-0 left-0 w-full h-[20%] px-5 md:px-10 flex justify-between items-between border-t rounded-b-lg border-color-secondary container-background-color"
@@ -33,6 +38,7 @@ const ToDoForm = () => {
 				placeholder="Enter to do task..." 
 				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
 				onChange={ (e) => { handleChange(e.target.value) } }
+                ref={textInput}
 			/>
 			<button type="submit" className="self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">
 				Add Task
