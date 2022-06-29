8fee861da86627b7cdfad355aa0f04e6ad560190
Stewart McFarlane <stewart@toru.digital>
2022-06-29 20:32:41 +0100

    Modal styles and auto focus the text input on component mount

--- src/components/enter-name-modal/EnterNameModal.js
+++ src/components/enter-name-modal/EnterNameModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { setUserName } from '../../redux/actions/user'
 
@@ -6,6 +6,8 @@ import { setUserName } from '../../redux/actions/user'
 const EnterNameModal = () => {
 	const dispatch = useDispatch()
 	const userName = useSelector(state => state.userName)
+	const textInput = useRef(null)
+
 
 	let nameDisplay = ''
 
@@ -17,13 +19,18 @@ const EnterNameModal = () => {
 		dispatch(setUserName(userName))
 	}
 
+	useEffect(() => {
+		textInput.current.focus()
+	}, [])
+
 	return (
-	<form className="absolute top-0 left-0 rounded-lg border border-color-secondary h-full w-full" onSubmit={() => handleSubmit(nameDisplay)}>
+	<form className="absolute top-0 left-0 flex justify-center items-center rounded-lg border border-color-secondary h-full w-full" onSubmit={() => handleSubmit(nameDisplay)}>
 		<input 
-			className=" container-background-color text-5xl py-5"
+			className="container-background-color text-5xl py-5 focus:outline-0"
 			type="text" 
 			onChange={(event) => handleChange(event)}
 			placeholder="Enter your name..."
+			ref={textInput}
 		/>
 	</form>
 	)
