6b07f6479c8fb769151a7fece4c304cc254c8e94
Stewart McFarlane <stewart@toru.digital>
2022-06-29 19:55:27 +0100

    added fade animation to main container

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../../index.css'
 import '../../styles/global.scss'
+import Fade from 'react-reveal/Fade';
+
 
 import Header from '../header/Header'
 import ToDoList from '../to-do-list/ToDoList'
@@ -10,11 +12,13 @@ import ToDoForm from '../to-do-form/ToDoForm'
 const App = () => {
 	return (
 		<div className="background-primary w-screen h-screen flex justify-center items-center">
-			<main className="flex flex-col relative container-background-color w-[750px] max-w-[90%] h-[700px] max-h-[95vh] px-5 md:px-10 mx-auto border border-color-secondary rounded-lg text-center">
-				<Header />
-				<ToDoList/>
-				<ToDoForm />
-			</main>	
+            <Fade top duration={750}>
+                <main className="flex flex-col relative container-background-color w-[750px] max-w-[90%] h-[700px] max-h-[95vh] px-5 md:px-10 mx-auto border border-color-secondary rounded-lg text-center">
+                    <Header />
+                    <ToDoList/>
+                    <ToDoForm />
+                </main>	
+            </Fade>
 		</div>
 	)
 }
