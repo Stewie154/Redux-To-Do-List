51eb19a44046540a50e71e02f32d1b352186d35a
Stewart McFarlane <stewart@toru.digital>
2022-06-29 20:37:13 +0100

    fade header in after username input

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,13 @@ import React from 'react'
 import EnterNameModal from '../enter-name-modal/EnterNameModal'
 import { setUserName } from '../../redux/actions/user'
 import { useSelector, useDispatch } from 'react-redux'
+import Fade from 'react-reveal/Fade';
+
 
 const Header = () => {
 	const userName = useSelector(state => state.userName)
 
-	const renderContent = userName === '' ? <EnterNameModal /> : <h1 className="h-[20%] flex justify-center items-center text-3xl md:text-6xl underline ">{userName}'s To Do List</h1>
+	const renderContent = userName === '' ? <EnterNameModal /> : <Fade><h1 className="h-[20%] flex justify-center items-center text-3xl md:text-6xl underline ">{userName}'s To Do List</h1></Fade>
 
 	return (
 		<>
