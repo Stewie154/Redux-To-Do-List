ef28c7671a62ac75e20971c21481314250994b06
Stewart McFarlane <stewart@toru.digital>
2022-06-29 19:32:30 +0100

    render undo icon when to do item is completed

--- src/components/to-do-item/ToDoItem.js
+++ src/components/to-do-item/ToDoItem.js
@@ -19,7 +19,7 @@ const ToDoItem = ({ item }) => {
 		<p className={`text-lg md:text-2xl ${item.completed && 'line-through opacity-20'}`}>{item.title}</p>
 		<div className="flex">
 			<img 
-				src="/images/icons/checkmark-circle-outline.svg" 
+				src={item.completed ? '/images/icons/arrow-undo-outline.svg' : '/images/icons/checkmark-circle-outline.svg'}
 				alt="check icon"
 				className="w-6 md:w-8 cursor-pointer mr-3 transition-all duration-150 ease-out hover:ease-in hover:w-8 md:hover:w-10 hover:opacity-70"
                 onClick={() => toggleComplete(item.id)}
