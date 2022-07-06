8c3b8a6ae742f00b98598e7dcb577fb09195013d
Stewart McFarlane <stewart@toru.digital>
2022-07-06 21:43:31 +0100

    show selected to do title in text input on to do item click

--- src/components/to-do-form/ToDoForm.js
+++ src/components/to-do-form/ToDoForm.js
@@ -5,13 +5,21 @@ import { createToDo } from '../../redux/actions/toDos'
 
 const ToDoForm = () => {
 	const currentText = useSelector(state => state.currentText)
-    const userName = useSelector(state => state.userName)
+	const userName = useSelector(state => state.userName)
+	const selectedToDo = useSelector(state => state.selectedToDo)
+
 	const dispatch = useDispatch()
 
 	const textInput = useRef(null)
 
 	const handleChange = (textValue) => {
 		dispatch(updateText(textValue))
+	}
+
+	const handleEditText = (selectedToDo) => {
+		if (selectedToDo !== null) {
+			dispatch(updateText(selectedToDo.title))
+		}
 	}
 
 	const handleSubmit = (event, currentText) => {
@@ -25,19 +33,20 @@ const ToDoForm = () => {
 
 	useEffect(() => {
 		textInput.current.focus()
-	}, [userName])
+		handleEditText(selectedToDo)
+	}, [userName, selectedToDo])
 
 	return (
-		<form 
+		<form
 			className={`${userName === '' && 'hidden'} absolute bottom-0 left-0 w-full h-[20%] px-5 md:px-10 flex justify-between items-between border-t rounded-b-lg border-color-secondary container-background-color`}
 			onSubmit={(event) => handleSubmit(event, currentText)}
 		>
-			<input 
-				type="text" 
+			<input
+				type="text"
 				value={currentText}
-				placeholder="Enter to do task..." 
-				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0" 
-				onChange={ (e) => { handleChange(e.target.value) } }
+				placeholder="Enter to do task..."
+				className="w-9/12 bg-transparent border-0 text-lg md:text-2xl focus:outline-0"
+				onChange={(e) => { handleChange(e.target.value) }}
 				ref={textInput}
 			/>
 			<button type="submit" className="self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">
