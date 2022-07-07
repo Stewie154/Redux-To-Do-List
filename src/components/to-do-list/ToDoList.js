a7f7e29a499e66516146a09331f8cbc7cf099b57
Stewart McFarlane <stewart@toru.digital>
2022-07-07 20:26:51 +0100

    clear all to-dos functionality

--- src/components/to-do-list/ToDoList.js
+++ src/components/to-do-list/ToDoList.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { clearAllToDos } from '../../redux/actions/toDos'
 
 import ToDoItem from '../to-do-item/ToDoItem'
 
 const ToDoList = () => {
+	const dispatch = useDispatch()
 	const allToDos = useSelector(state => state.toDos)
-    const userName = useSelector(state => state.userName)
+	const userName = useSelector(state => state.userName)
+
+	const handleClearToDos = () => {
+		dispatch(clearAllToDos())
+	}
+
+	const renderClearButton = (
+		allToDos.length > 0 && 
+			<p 
+				className="w-fit ml-auto pb-2.5 underline italic opacity-90 tracking-wider text-right cursor-pointer hover:opacity-50"
+				onClick={handleClearToDos}
+			>
+				Clear To-do list
+			</p>
+	)
+
 	const renderList = allToDos.map((item, key) => {
 		return (
 			<ToDoItem item={item} key={key}/>
 		)
 	})
-	const renderClearButton = (
-			allToDos.length > 0 && 
-			<p className="w-fit ml-auto pb-2.5 underline italic opacity-90 tracking-wider text-right cursor-pointer hover:opacity-50">
-				Clear To-do list
-			</p>
-		)
+	
 
 	return (
 		<div className={`h-[60%] overflow-scroll ${userName === '' && 'hidden'}`}>
--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -3,6 +3,7 @@ export const CREATE_TODO = 'CREATE_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 export const DELETE_TODO = 'DELETE_TODO'
 export const EDIT_TODO = 'EDIT_TODO'
+export const CLEAR_ALL_TODOS = 'CLEAR_ALL_TODOS'
 export const SELECT_TODO = 'SELECT_TODO'
 export const DESELECT_TODO = 'DESELECT_TODO'
 
