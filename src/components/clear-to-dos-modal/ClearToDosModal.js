39fa999ba63963d916d2323dd34e990afcb377cf
Stewart McFarlane <stewart@toru.digital>
2022-12-28 19:10:32 +0000

    modal styles

--- src/components/clear-to-dos-modal/ClearToDosModal.js
+++ src/components/clear-to-dos-modal/ClearToDosModal.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './ClearToDosModal.scss'
 
 const ClearToDosModal = () => {
 	return (
-		<div className="absolute bg-red-500 top-0 left-0 flex flex-col justify-center items-center rounded-lg border border-color-secondary h-full w-full">
-			clear modal here
+		<div className="clear-to-dos-modal absolute z-20 top-0 left-0 flex flex-col justify-center items-center rounded-lg border border-color-secondary h-full w-full">
+			<main className="w-8/12 h-8/12 flex justify-center items-center">
+				<section className="flex justify-center items-center">
+					<button className="mx-3 self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">Yes</button>
+					<button className="mx-3 self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">No</button>
+				</section>
+			</main>
 		</div>
 	)
 }
