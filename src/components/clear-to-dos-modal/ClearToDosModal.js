bace3bbe81dc8911413f8db1aef5effd22da8db2
Stewart McFarlane <stewart@toru.digital>
2022-12-28 19:15:27 +0000

    added text to modal

--- src/components/clear-to-dos-modal/ClearToDosModal.js
+++ src/components/clear-to-dos-modal/ClearToDosModal.js
@@ -4,7 +4,8 @@ import './ClearToDosModal.scss'
 const ClearToDosModal = () => {
 	return (
 		<div className="clear-to-dos-modal absolute z-20 top-0 left-0 flex flex-col justify-center items-center rounded-lg border border-color-secondary h-full w-full">
-			<main className="w-8/12 h-8/12 flex justify-center items-center">
+			<main className="w-8/12 h-8/12 flex flex-col justify-center items-center">
+				<h3 className="mb-10 text-lg sm:text-xl md:text-2xl">Are you sure you want to clear this list?</h3>
 				<section className="flex justify-center items-center">
 					<button className="mx-3 self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">Yes</button>
 					<button className="mx-3 self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70">No</button>
