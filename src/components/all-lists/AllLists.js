3333ebb671d13f89bde26191a145d77aec73bd05
Stewart McFarlane <stewart@toru.digital>
2023-01-02 17:55:31 +0000

    grabbed lists from redux store and console logged them in AllLists component

--- src/components/all-lists/AllLists.js
+++ src/components/all-lists/AllLists.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import ListItem from '../list-item/ListItem'
 
 const AllLists = () => {
+
+	const dispatch = useDispatch()
+
+	const lists = useSelector(state => state.lists)
+
+	console.log(lists)
 	return (
 		<div>All Lists component here:
 			<ListItem title="list item here" id={1}/>
