a949893845dc1b188842930f57c24a4c28e2cc19
Stewart McFarlane <stewart@toru.digital>
2023-01-02 17:07:04 +0000

    created ListItem component & rendered in AllLists

--- src/components/all-lists/AllLists.js
+++ src/components/all-lists/AllLists.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import ListItem from '../list-item/ListItem'
 
 const AllLists = () => {
 	return (
-		<div>All Lists component here</div>
+		<div>All Lists component here:
+			<ListItem />
+			<ListItem />
+			<ListItem />
+		</div>
 	)
 }
 
