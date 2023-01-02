b531a8ee0485372762934ec2ab7e0a8418a3aca7
Stewart McFarlane <stewart@toru.digital>
2023-01-02 17:51:01 +0000

    created listsReducer & added CREATE_LIST action type to switch statement

---
+++ src/redux/reducers/listsReducer.js
@@ -0,0 +1,39 @@
+import {
+	CREATE_LIST
+} from '../actions/types'
+
+initialState = [
+	{
+		id: 1, 
+		title: 'Example List 1',
+		selected: false,
+		items: [
+			
+		]
+	},
+	{
+		id: 2, 
+		title: 'Example List 2',
+		selected: false,
+		items: [
+			
+		]
+	},
+	{
+		id: 1, 
+		title: 'Example List 3',
+		selected: false,
+		items: [
+			
+		]
+	}
+]
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case CREATE_LIST:
+			return [...state, action.payload]
+		default:
+			return state
+	}
+}
