a2ef5df7ebcd321e742d4e84e823a0a6e364ebd9
Stewart McFarlane <stewart@toru.digital>
2023-01-02 16:58:48 +0000

    created AllLists component

---
+++ src/components/all-lists/AllLists.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllLists = () => {
+	return (
+		<div>All Lists component here</div>
+	)
+}
+
+export default AllLists
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,6 +6,7 @@ import Fade from 'react-reveal/Fade';
 
 import Header from '../header/Header'
 import ToDoList from '../to-do-list/ToDoList'
+import AllLists from '../all-lists/AllLists';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
 			<Fade top duration={750}>
 				<main className="flex flex-col relative container-background-color w-[750px] max-w-[90%] h-[700px] max-h-[95vh] px-5 md:px-10 mx-auto border border-color-secondary rounded-lg text-center">
 					<Header />
+					<AllLists />
 					<ToDoList />
 				</main>
 			</Fade>
