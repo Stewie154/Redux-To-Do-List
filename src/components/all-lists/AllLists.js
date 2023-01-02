5266c1bf5d436eb986feb36d3ff2d949837264b9
Stewart McFarlane <stewart@toru.digital>
2023-01-02 17:25:37 +0000

    rendered ListItem content

--- src/components/all-lists/AllLists.js
+++ src/components/all-lists/AllLists.js
@@ -4,9 +4,9 @@ import ListItem from '../list-item/ListItem'
 const AllLists = () => {
 	return (
 		<div>All Lists component here:
-			<ListItem />
-			<ListItem />
-			<ListItem />
+			<ListItem title="list item here" id={1}/>
+			<ListItem title="list item here" id={2}/>
+			<ListItem title="list item here" id={3}/>
 		</div>
 	)
 }
