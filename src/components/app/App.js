9d21e18b5ffe2d38b57f409421652ceba4b2f380
Stewart McFarlane <stewart@toru.digital>
2023-01-02 21:17:58 +0000

    render list of lists when no individual list is selected

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,19 +2,24 @@ import React from 'react'
 import '../../index.css'
 import '../../styles/global.scss'
 import Fade from 'react-reveal/Fade';
+import { useSelector } from 'react-redux';
 
 import SelectedList from '../selected-list/SelectedList'
 import AllLists from '../all-lists/AllLists';
 
 
 const App = () => {
+	const selectedList = useSelector(state => state.selectedList)
+
+	const renderContent = () => {
+		return selectedList === null ? <AllLists /> : <SelectedList />
+	}
+
 	return (
 		<div className="background-primary w-screen h-screen flex justify-center items-center">
 			<Fade top duration={750}>
 				<main className="flex flex-col relative container-background-color w-[750px] max-w-[90%] h-[700px] max-h-[95vh] px-5 md:px-10 mx-auto border border-color-secondary rounded-lg text-center">
-					{/* when state is ready, use ternary to render AllLists when no list is selected  */}
-					<AllLists />
-					<SelectedList />
+					{renderContent()}
 				</main>
 			</Fade>
 		</div>
