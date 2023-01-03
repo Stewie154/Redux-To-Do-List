0175039e05e1b692399e5878626da67148298766
Stewart McFarlane <stewart@toru.digital>
2023-01-03 20:48:07 +0000

    removed hard coded lists from state

--- src/redux/reducers/listsReducer.js
+++ src/redux/reducers/listsReducer.js
@@ -3,32 +3,7 @@ import {
 	DELETE_LIST
 } from '../actions/types'
 
-let initialState = [
-	{
-		id: 1, 
-		title: 'Example List 1',
-		selected: false,
-		items: [
-			
-		]
-	},
-	{
-		id: 2, 
-		title: 'Example List 2',
-		selected: false,
-		items: [
-			
-		]
-	},
-	{
-		id: 3, 
-		title: 'Example List 3',
-		selected: false,
-		items: [
-			
-		]
-	}
-]
+let initialState = []
 
 export default (state = initialState, action) => {
 	switch (action.type) {
