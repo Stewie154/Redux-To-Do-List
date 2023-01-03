c57524cd106b2929c07eddf241d6c01111502b57
Stewart McFarlane <stewart@toru.digital>
2023-01-03 20:33:18 +0000

    create deleteList action & added to listsReducer switch statement

--- src/redux/actions/lists.js
+++ src/redux/actions/lists.js
@@ -1,8 +1,15 @@
-import { CREATE_LIST } from "./types";
+import { CREATE_LIST, DELETE_LIST } from "./types";
 
 export const createList = (list) => {
 	return {
 		type: CREATE_LIST,
 		payload: list
+	}
+}
+
+export const deleteList = (listId) => {
+	return {
+		type: DELETE_LIST,
+		payload: listId
 	}
 }
--- src/redux/reducers/listsReducer.js
+++ src/redux/reducers/listsReducer.js
@@ -1,5 +1,6 @@
 import {
-	CREATE_LIST
+	CREATE_LIST,
+	DELETE_LIST
 } from '../actions/types'
 
 let initialState = [
@@ -33,6 +34,9 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 		case CREATE_LIST:
 			return [...state, action.payload]
+		case DELETE_LIST:
+			let newList = state.filter(item => item.id !== action.payload)
+			return newList
 		default:
 			return state
 	}
