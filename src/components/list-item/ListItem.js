8bf85e5cdbd1e3df7abc84c9e4f95a05c8feeaaa
Stewart McFarlane <stewart@toru.digital>
2023-01-04 09:11:53 +0000

    passed whole list object into ListItem

--- src/components/list-item/ListItem.js
+++ src/components/list-item/ListItem.js
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux'
 import { toggleListsModal } from '../../redux/actions/listsModal';
 
 
-const ListItem = ({ title, id }) => {
+const ListItem = ({ list }) => {
 
 	const dispatch = useDispatch()
 
@@ -17,21 +17,21 @@ const ListItem = ({ title, id }) => {
 	return (
 		<Fade top duration={500}>
 			<div className="flex justify-between items-center py-2">
-				<p className={`text-lg text-left max-w-[70%] sm:max-w-full md:text-2xl hover:opacity-60 cursor-pointer`} onClick={() => console.log(title)}>
-					{title}
+				<p className={`text-lg text-left max-w-[70%] sm:max-w-full md:text-2xl hover:opacity-60 cursor-pointer`} onClick={() => console.log(list.title)}>
+					{list.title}
 				</p>
 				<div className="flex">
 					<img
 						src="/images/icons/pencil.svg"
 						alt="check icon"
 						className="w-6 md:w-8 cursor-pointer mr-3 transition-all duration-150 ease-out hover:ease-in hover:w-8 md:hover:w-10 hover:opacity-70"
-						onClick={() => console.log(id)}
+						onClick={() => console.log(list.id)}
 					/>
 					<img
 						src="/images/icons/trash-outline.svg"
 						alt="trash icon"
 						className="w-6 md:w-8 cursor-pointer transition-all duration-150 ease-out hover:ease-in hover:w-8 md:hover:w-10 hover:opacity-70"
-						onClick={() => handleDeleteClick(id)}
+						onClick={() => handleDeleteClick(list.id)}
 					/>
 				</div>
 			</div>
