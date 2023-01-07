45d606f061bdef83dceabdc5a6692ca6aca48550
Stewart McFarlane <stewart@toru.digital>
2023-01-07 17:26:20 +0000

    updated name 'title update' to 'title edit' for consistency

--- src/redux/actions/listEditingTitle.js
+++ src/redux/actions/listEditingTitle.js
@@ -1,14 +1,14 @@
-import { SELECT_LIST_FOR_TITLE_UPDATE, DESELECT_LIST_FOR_TITLE_UPDATE } from "./types"
+import { SELECT_LIST_FOR_TITLE_EDIT, DESELECT_LIST_FOR_TITLE_EDIT } from "./types"
 
-export const selectListForTitleUpdate = (list) => {
+export const selectListForTitleEdit = (list) => {
 	return {
-		type: SELECT_LIST_FOR_TITLE_UPDATE,
+		type: SELECT_LIST_FOR_TITLE_EDIT,
 		payload: list
 	}
 }
 
-export const deselectListForTitleUpdate = () => {
+export const deselectListForTitleEdit = () => {
 	return {
-		type: DESELECT_LIST_FOR_TITLE_UPDATE,
+		type: DESELECT_LIST_FOR_TITLE_EDIT,
 	}
 }
--- src/redux/reducers/listEditingTitleReducer.js
+++ src/redux/reducers/listEditingTitleReducer.js
@@ -1,12 +1,12 @@
-import { SELECT_LIST_FOR_TITLE_UPDATE, DESELECT_LIST_FOR_TITLE_UPDATE } from "../actions/types";
+import { SELECT_LIST_FOR_TITLE_EDIT, DESELECT_LIST_FOR_TITLE_EDIT } from "../actions/types";
 
 let initialState = null
 
 export default (state = initialState, action) => {
 	switch (action.type) {
-		case SELECT_LIST_FOR_TITLE_UPDATE:
+		case SELECT_LIST_FOR_TITLE_EDIT:
 			return action.payload
-		case DESELECT_LIST_FOR_TITLE_UPDATE:
+		case DESELECT_LIST_FOR_TITLE_EDIT:
 			return null
 		default:
 			return state
