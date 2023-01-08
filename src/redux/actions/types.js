59e8d56ebe327d700d06d693817fac92ec9973c2
Stewart McFarlane <stewart@toru.digital>
2023-01-08 14:25:22 +0000

    removed redundant action types

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -22,15 +22,6 @@ export const DESELECT_LIST = 'DESELECT_LIST'
 export const SELECT_LIST_ITEM = 'SELECT_LIST_ITEM'
 export const DESELECT_LIST_ITEM = 'DESELECT_LIST_ITEM'
 
-// To do action types 
-export const CREATE_TODO = 'CREATE_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
-export const DELETE_TODO = 'DELETE_TODO'
-export const EDIT_TODO = 'EDIT_TODO'
-export const CLEAR_ALL_TODOS = 'CLEAR_ALL_TODOS'
-export const SELECT_TODO = 'SELECT_TODO'
-export const DESELECT_TODO = 'DESELECT_TODO'
-
 // To do form action types
 export const UPDATE_TEXT = 'UPDATE_TEXT'
 
