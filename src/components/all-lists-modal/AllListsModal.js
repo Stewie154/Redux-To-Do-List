c5af955db97f3f33f197b1c01fc85b266d685560
Stewart McFarlane <stewart@toru.digital>
2023-01-08 19:57:47 +0000

    removed nested h3 tag dom warning

--- src/components/all-lists-modal/AllListsModal.js
+++ src/components/all-lists-modal/AllListsModal.js
@@ -22,12 +22,12 @@ const AllListsModal = () => {
 		dispatch(toggleListsModal())
 	}
 
-	const text = clickedItemId === null ? <h3>Are you sure you want to <span className="italic text-red-500">delete all lists</span> and all of their contents?</h3>: <h3>Are you sure you want to <span className="italic text-red-500">delete this list</span> and all of it's contents?</h3>
+	const text = clickedItemId === null ? <h3 className="mb-10 text-lg sm:text-xl md:text-2xl">Are you sure you want to <span className="italic text-red-500">delete all lists</span> and all of their contents?</h3>: <h3 className="mb-10 text-lg sm:text-xl md:text-2xl">Are you sure you want to <span className="italic text-red-500">delete this list</span> and all of it's contents?</h3>
 
 	return (
 		<div className="modal-background-color absolute z-20 top-0 left-0 flex flex-col justify-center items-center rounded-lg border border-color-secondary h-full w-full">
 			<main className="w-8/12 h-8/12 flex flex-col justify-center items-center">
-				<h3 className="mb-10 text-lg sm:text-xl md:text-2xl">{text}</h3>
+				{text}
 				<section className="flex justify-center items-center">
 					<button 
 						className="mx-3 self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70"
