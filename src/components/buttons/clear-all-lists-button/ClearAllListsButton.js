5cc7105a14ba4088a65217b6e503ada81c6d8945
Stewart McFarlane <stewart@toru.digital>
2023-01-09 08:26:33 +0000

    removed padding

--- src/components/buttons/clear-all-lists-button/ClearAllListsButton.js
+++ src/components/buttons/clear-all-lists-button/ClearAllListsButton.js
@@ -15,7 +15,7 @@ const ClearAllListsButton = () => {
 
 	return (
 			<p 
-				className={`${lists.length > 1 & userName !== '' ? 'w-fit ml-auto pb-2.5 italic opacity-90 tracking-wider text-right cursor-pointer hover:opacity-50 hover:underline' : 'hidden'}`}
+				className={`${lists.length > 1 & userName !== '' ? 'w-fit ml-auto italic opacity-90 tracking-wider text-right cursor-pointer hover:opacity-50 hover:underline' : 'hidden'}`}
 				onClick={() => handleClearAllClick()}
 			>
 				Clear all lists
