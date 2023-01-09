8ab23cd9c9e04bb3e4a1896603d162428e1d7193
Stewart McFarlane <stewart@toru.digital>
2023-01-09 08:42:55 +0000

    red warning styles in ClearListItemsModal

--- src/components/clear-list-items-modal/ClearListItemsModal.js
+++ src/components/clear-list-items-modal/ClearListItemsModal.js
@@ -16,10 +16,12 @@ const ClearListItemsModal = () => {
 		dispatch(toggleListItemsModal())
 	}
 
+	const text = <h3 className="mb-10 text-lg sm:text-xl md:text-2xl">Are you sure you want to <span className="italic text-red-500">clear all items</span> from this list?</h3>
+
 	return (
 		<div className="modal-background-color absolute z-20 top-0 left-0 flex flex-col justify-center items-center rounded-lg border border-color-secondary h-full w-full">
 			<main className="w-8/12 h-8/12 flex flex-col justify-center items-center">
-				<h3 className="mb-10 text-lg sm:text-xl md:text-2xl">Are you sure you want to clear this list?</h3>
+				{text}
 				<section className="flex justify-center items-center">
 					<button 
 						className="mx-3 self-center border border-color-secondary py-2 px-4 rounded-lg transition-all duration-75 ease-out hover:ease-in hover:opacity-70"
