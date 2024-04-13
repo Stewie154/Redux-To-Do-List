fad3cbd17517c92e331cba2c3b1ec7fa9e768cf6
Stewart <69080603+Stewie154@users.noreply.github.com>
2024-04-13 11:12:03 +0100

    created userSlice & added to the store

--- src/redux-toolkit/store/store.js
+++ src/redux-toolkit/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import listsReducer from "../slices/listsSlice";
+import userReducer from "../slices/userSlice";
 
 export const store = configureStore({
 	reducer: {
+		user: userReducer,
 		lists: listsReducer,
 	},
 });
