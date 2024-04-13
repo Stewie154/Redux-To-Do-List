2f1a328053fe76bddc64a87c3784d028a00051f0
Stewart <69080603+Stewie154@users.noreply.github.com>
2024-04-13 10:40:16 +0100

    created listsSlice & added reducer to store

---
+++ src/redux-toolkit/slices/listsSlice.js
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+	{
+		id: 0,
+		title: "List One Title",
+		items: [
+			{
+				id: 0,
+				title: "List one, item 1",
+				completed: false,
+			},
+			{
+				id: 1,
+				title: "List one, item 2",
+				completed: false,
+			},
+			{
+				id: 2,
+				title: "List one, item 3",
+				completed: false,
+			},
+		],
+	},
+	{
+		id: 1,
+		title: "List Two Title",
+		items: [
+			{
+				id: 0,
+				title: "List Two, item 1",
+				completed: false,
+			},
+			{
+				id: 1,
+				title: "List Two, item 2",
+				completed: false,
+			},
+			{
+				id: 2,
+				title: "List Two, item 3",
+				completed: false,
+			},
+		],
+	},
+	{
+		id: 0,
+		title: "List Three Title",
+		items: [
+			{
+				id: 0,
+				title: "List Three, item 1",
+				completed: false,
+			},
+			{
+				id: 1,
+				title: "List Three, item 2",
+				completed: false,
+			},
+			{
+				id: 2,
+				title: "List Three, item 3",
+				completed: false,
+			},
+		],
+	},
+];
+
+export const listsSlice = createSlice({
+	name: "lists",
+	initialState,
+	reducers: {},
+});
+
+export default listsSlice.reducer;
