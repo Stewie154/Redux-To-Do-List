b11d71b8d710b6eb03d03a59f6ea1a6458e05eb9
Stewart <69080603+Stewie154@users.noreply.github.com>
2024-04-13 10:46:05 +0100

    rendered app with redux toolkit store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom'
-import { Provider } from 'react-redux'
-import  {store, Persister} from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store, Persister } from "./redux/store";
+import { store as toolkitStore } from "./redux-toolkit/store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-import App from './components/app/App'
+import App from "./components/app/App";
+
+//uses vaniliia redux
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <PersistGate loading={<div>Loading...</div>} persistor={Persister}>
+//             <App/>
+//         </PersistGate>
+//     </Provider>, document.querySelector('#root')
+// )
+
+//uses redux toolkit
 
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={<div>Loading...</div>} persistor={Persister}>
-            <App/>
-        </PersistGate>
-    </Provider>, document.querySelector('#root')
-)
+	<Provider store={toolkitStore}>
+		<App />
+	</Provider>,
+	document.querySelector("#root"),
+);
