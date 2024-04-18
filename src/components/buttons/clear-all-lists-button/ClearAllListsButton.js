82b2e152f46a821e2ad6bdea0cd2529256cea80b
Stewart <69080603+Stewie154@users.noreply.github.com>
2024-04-18 08:56:41 +0100

    deleteAllLists action working

--- src/components/buttons/clear-all-lists-button/ClearAllListsButton.js
+++ src/components/buttons/clear-all-lists-button/ClearAllListsButton.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import Fade from 'react-reveal/Fade';
-import { useSelector, useDispatch } from 'react-redux'
-import { toggleListsModal } from '../../../redux/actions/listsModal'
-
+import React from "react";
+import Fade from "react-reveal/Fade";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleListModal } from "../../../redux-toolkit/slices/modalsSlice";
 
 const ClearAllListsButton = () => {
-
-	const dispatch = useDispatch()
-	const lists = useSelector(state => state.lists)
-	const userName = useSelector(state => state.username)
+	const dispatch = useDispatch();
+	const lists = useSelector((state) => state.lists);
+	const userName = useSelector((state) => state.username);
 
 	const handleClearAllClick = () => {
-		dispatch(toggleListsModal())
-	}
+		dispatch(toggleListModal());
+	};
 
 	return (
 		<Fade top duration={850}>
 			<p
-				className={`${lists.length > 1 & userName !== '' ? 'w-fit ml-auto italic opacity-90 tracking-wider text-right cursor-pointer hover:opacity-50 hover:underline' : 'hidden'}`}
+				className={`${
+					(lists.length > 1) & (userName !== "")
+						? "w-fit ml-auto italic opacity-90 tracking-wider text-right cursor-pointer hover:opacity-50 hover:underline"
+						: "hidden"
+				}`}
 				onClick={() => handleClearAllClick()}
 			>
 				Clear all lists
 			</p>
 		</Fade>
+	);
+};
 
-	)
-}
-
-export default ClearAllListsButton
+export default ClearAllListsButton;
